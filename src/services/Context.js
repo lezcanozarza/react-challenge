f3ce8c212a410bea7410b4dfb664ea2fa211aaa8
lezcanozarza <lezcanozarza@gmail.com>
2021-09-29 15:38:48 -0300

    filter bugs fixed

--- src/services/Context.js
+++ src/services/Context.js
@@ -25,17 +25,14 @@ export const DataProvider = ({ children }) => {
 
   const getBikesFiltered = (data) => {
     setLoader(true);
-    if (data.title) {
       //if there are only title parameters
       fetch(
-        "https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=Berlin&distance=100&stolenness=proximity"
+        `https://bikeindex.org:443/api/v3/search?page=1&per_page=100&query=${data.title}&location=Berlin&distance=100&stolenness=proximity`
       )
         .then((response) => response.json())
         .then((response) => {
           setLoader(false);
-          let bikesFiltered = response.bikes.filter((bike) => {
-            return bike.title.includes(data.title);
-          });
+          let bikesFiltered = response.bikes
           if (data.dateFrom && data.dateTo) {
           //if there are title and date parameters, we filter from the previously filtered variable 'bikesFiltered'
             bikesFiltered = bikesFiltered.filter((bike) => {
@@ -53,27 +50,27 @@ export const DataProvider = ({ children }) => {
           setBikes("Error");
           setLoader(false);
         });
-    }
 
-    if (!data.title && data.dateFrom && data.dateTo) {
-      //if there are only date search parameters
-      fetch(
-        "https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=Berlin&distance=100&stolenness=proximity"
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(data.dateFro, data.dateTo);
-          let bikesFiltered = response.bikes.filter((bike) => {
 
-            //I use the moment library to compare the dates and filter
-            const stolenDate = new Date(
-              bike.date_stolen * 1000
-            ).toLocaleDateString("en-US");
-            return moment(stolenDate).isBetween(data.dateFrom, data.dateTo);
-          });
-          setBikes(bikesFiltered);
-        });
-    }
+    // if (!data.title && data.dateFrom && data.dateTo) {
+    //   //if there are only date search parameters
+    //   fetch(
+    //     "https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=Berlin&distance=100&stolenness=proximity"
+    //   )
+    //     .then((response) => response.json())
+    //     .then((response) => {
+    //       console.log(data.dateFro, data.dateTo);
+    //       let bikesFiltered = response.bikes.filter((bike) => {
+    //
+    //         //I use the moment library to compare the dates and filter
+    //         const stolenDate = new Date(
+    //           bike.date_stolen * 1000
+    //         ).toLocaleDateString("en-US");
+    //         return moment(stolenDate).isBetween(data.dateFrom, data.dateTo);
+    //       });
+    //       setBikes(bikesFiltered);
+    //     });
+    // }
   };
 
   return (
